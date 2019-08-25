9d34389847b1ba14358b110b473fadb1be87a7e1
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2019-08-25 04:50:57 +0100

    Added basic CRUD handlers

--- index.js
+++ index.js
@@ -6,12 +6,47 @@ const promisify = require("util").promisify;
 const port = 3000;
 const host = "localhost";
 
+const CRUD_Handlers = {
+    POST: handlePOST,
+    GET: handleGET,
+    PUT: handlePUT,
+    DELETE: handleDELETE 
+};
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World");
+    if (Object.keys(CRUD_Handlers).includes(req.method)) {
+        CRUD_Handlers[req.method.toUpperCase()](req, res);
+    }else{
+        res.statusCode = 405;                                                                       
+        res.setHeader("Content-Type", "text/plain");
+        res.end(`${req.method} requests are not allowed`);
+    }
 })
 
 server.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}/`);
 });
+
+function handlePOST(req, res) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("You made a POST request");
+}
+
+function handleGET(req, res) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("You made a GET request");
+}
+
+function handlePUT(req, res) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("You made a PUT request");
+}
+
+function handleDELETE(req, res) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("You made a DELETE request");
+}
